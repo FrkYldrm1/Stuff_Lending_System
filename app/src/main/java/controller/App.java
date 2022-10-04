d5ce4d52a3b44ce19cd4397557c905b4bade4b95
Philip Olsson <po222jb@student.lnu.se>
2022-10-04 16:50:01 +0200

    resolved conflicts

--- app/src/main/java/controller/App.java
+++ app/src/main/java/controller/App.java
@@ -1,24 +1,9 @@
 package controller;
 
-<<<<<<< app/src/main/java/controller/App.java
-/**
- * Responsible for staring the application.
- */
-public class App {
-  /**
-   * Application starting point.
-
-   * @param args command line arguments.
-   */
-  public static void main(String[] args) {
-    // adapt to start the application in your way
-  }
-=======
 import model.Registry;
 
 public class App {
 	public static void main(String[] args) {
 		model.Registry registry = new Registry();
 	}
->>>>>>> app/src/main/java/controller/App.java
 }
